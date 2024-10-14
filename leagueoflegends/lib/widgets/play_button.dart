d8ca2ba0d3addffdef3b40f76d09b5f35bf99949
DM <944095635@qq.com>
2024-10-15 04:25:16 +0800

    PlayButtonPainter 优化

--- leagueoflegends/lib/widgets/play_button.dart
+++ leagueoflegends/lib/widgets/play_button.dart
@@ -9,6 +9,36 @@ import 'package:path_drawing/path_drawing.dart';
 final Path playPath = parseSvgPathData(
     'M107.405,-0.00005100000000002325L0.499885955,0.024332999999999994L-0.458643,0.02455099999999999L0.40061009999999997,1.255481Q4.65908,7.66886,4.65908,15.706Q4.65908,23.7432,0.4006321,30.1566L-0.18832300000000002,31.0002L107.413,30.9809L121.169,15.495L107.405,-0.00005100000000002325ZM1.68959,29.9998L106.964,29.9809L119.831,15.495L106.956,1.000051L1.44415,1.024117Q5.65907,7.56777,5.65908,15.706Q5.65908,23.6038,1.68959,29.9998Z');
 
+final Gradient playColor = Gradient.linear(
+  Offset.zero,
+  const Offset(0, 30),
+  [
+    const Color(0X883FE7FF),
+    const Color(0X88006D7D),
+    const Color(0X880493A7),
+  ],
+  [
+    0,
+    .5,
+    1,
+  ],
+);
+
+final playHoverColor = Gradient.linear(
+  Offset.zero,
+  const Offset(0, 30),
+  [
+    const Color(0xFFAFF5FF),
+    const Color(0xFF46E6FF),
+    const Color(0xFF00ADD4),
+  ],
+  [
+    0,
+    .5,
+    1,
+  ],
+);
+
 /// 开始按钮
 class PlayButton extends StatefulWidget {
   const PlayButton({
@@ -30,10 +60,6 @@ class _PlayButtonState extends State<PlayButton>
     with SingleTickerProviderStateMixin {
   // 动画控制器
   late AnimationController _controller;
-  late Animation<double> _animation;
-
-  /// 光斑
-  final List<PlayButtonlight> _lights = List.empty(growable: true);
 
   // 是否悬浮鼠标
   bool hover = false;
@@ -41,42 +67,10 @@ class _PlayButtonState extends State<PlayButton>
   @override
   void initState() {
     super.initState();
-
-    init();
-
     _controller = AnimationController(
-      duration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 100),
       vsync: this,
     );
-
-    _animation = Tween<double>(
-      begin: 1,
-      end: 0,
-    ).animate(CurvedAnimation(
-      parent: _controller,
-      curve: Curves.slowMiddle,
-    ));
-  }
-
-  void init() async {
-    for (var i = 0; i < 20; i++) {
-      double scale = Random().nextDouble() * 3 + 2;
-      _lights.add(
-        PlayButtonlight(
-          x: i * 13,
-          y: 6,
-          scale: scale,
-        ),
-      );
-      _lights.add(
-        PlayButtonlight(
-          x: i * 12,
-          y: 23,
-          scale: scale,
-        ),
-      );
-      await Future.delayed(const Duration(milliseconds: 10));
-    }
   }
 
   @override
@@ -133,23 +127,11 @@ class _PlayButtonState extends State<PlayButton>
                       child: SizedBox(
                         width: 120,
                         height: 30,
-                        child: AnimatedBuilder(
-                          animation: _animation,
-                          builder: (context, child) {
-                            //debugPrint("重绘");
-                            for (var light in _lights) {
-                              light.doMove();
-                            }
-                            return CustomPaint(
-                              painter: PlayButtonPainter(
-                                _animation.value,
-                                _lights,
-                              ),
-                              child: Center(
-                                child: widget.text,
-                              ),
-                            );
-                          },
+                        child: CustomPaint(
+                          painter: PlayButtonPainter(_controller),
+                          child: Center(
+                            child: widget.text,
+                          ),
                         ),
                       ),
                     ),
@@ -169,61 +151,54 @@ class _PlayButtonState extends State<PlayButton>
   }
 }
 
-final Gradient playColor = Gradient.linear(
-  Offset.zero,
-  const Offset(0, 30),
-  [
-    const Color(0X883FE7FF),
-    const Color(0X88006D7D),
-    const Color(0X880493A7),
-  ],
-  [
-    0,
-    .5,
-    1,
-  ],
-);
-
-final playHoverColor = Gradient.linear(
-  Offset.zero,
-  const Offset(0, 30),
-  [
-    const Color(0xFFAFF5FF),
-    const Color(0xFF46E6FF),
-    const Color(0xFF00ADD4),
-  ],
-  [
-    0,
-    .5,
-    1,
-  ],
-);
-
 class PlayButtonPainter extends CustomPainter {
-  const PlayButtonPainter(
-    this.progress,
-    this._lights,
-  );
+  PlayButtonPainter(this.factor) : super(repaint: factor) {
+    //debugPrint("PlayButtonPainter 初始化");
+    for (var i = 0; i < 20; i++) {
+      double scale = Random().nextDouble() * 3 + 2;
+      _lights.add(
+        PlayButtonlight(
+          x: i * 10,
+          y: 6,
+          scale: scale,
+        ),
+      );
+      _lights.add(
+        PlayButtonlight(
+          x: i * 9,
+          y: 23,
+          scale: scale,
+        ),
+      );
+      Future.delayed(const Duration(milliseconds: 10));
+    }
+  }
 
-  /// 动画进度
-  final double progress;
+  /// 动画控制
+  final Animation<double> factor;
 
   /// 光斑
-  final List<PlayButtonlight> _lights;
+  final List<PlayButtonlight> _lights = List.empty(growable: true);
 
   @override
   void paint(Canvas canvas, Size size) {
+    for (var light in _lights) {
+      light.doMove();
+    }
+
     // 绘制顶部的箭头形状
     Path arrowPath = Path();
     arrowPath.addPath(playPath, Offset(size.width - 120, -0.5));
 
+    /// 判断动画是否启动
+    //debugPrint("判断动画是否启动");
     Paint arrowPaint = Paint()
-      ..shader = progress == 1 ? playColor : playHoverColor
+      ..shader = factor.isAnimating ? playHoverColor : playColor
       ..style = PaintingStyle.stroke
       ..strokeWidth = 1.5;
     canvas.drawPath(arrowPath, arrowPaint);
 
-    if (progress < 1) {
+    if (factor.isAnimating) {
       var paint = Paint()
         ..style = PaintingStyle.fill
         ..color = const Color.fromARGB(200, 0, 195, 234)
@@ -246,7 +221,7 @@ class PlayButtonPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant PlayButtonPainter oldDelegate) {
-    return progress != oldDelegate.progress;
+    return factor != oldDelegate.factor;
   }
 }
 
